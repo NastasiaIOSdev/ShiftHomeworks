d8198b51e8ec92801453fb1b8e5b175233ccb6fd
AnastasiiaLarina <laranweb@gmail.com>
2022-05-04 15:21:32 +0500

    Изменения

--- Homework-2/Homework-2/main.swift
+++ Homework-2/Homework-2/main.swift
@@ -11,16 +11,12 @@ var safeArray = ThreadSafeArray<Int>()
 
 let queue = DispatchQueue(label: "Save", attributes: .concurrent)
 
-let semafore = DispatchSemaphore(value: 1)
-
 let group1 = DispatchGroup()
 
 group1.enter()
 queue.async {
     for number in 0...1000 {
-        semafore.wait()
         safeArray.append(number)
-        semafore.signal()
     }
     group1.leave()
 }
@@ -28,9 +24,7 @@ queue.async {
 group1.enter()
 queue.async {
     for number in 0...1000 {
-        semafore.wait()
         safeArray.append(number)
-        semafore.signal()
     }
     group1.leave()
 }
