ba85d3aaa37f19c5f38aeceff76a6039a7d9b26a
AnastasiiaLarina <laranweb@gmail.com>
2022-05-06 00:01:52 +0500

    Создание TabBar

--- Homework-3/Homework-3/HardSkillsViewController.swift
+++ Homework-3/Homework-3/HardSkillsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HardSkillsViewController.swift
 //  Homework-3
 //
 //  Created by Анастасия Ларина on 05.05.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HardSkillsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(displayP3Red: 241/255, green: 221/255, blue: 201/255, alpha: 1)
+        title = "Hard skills :"
     }
-
-
 }
-
---
+++ Homework-3/Homework-3/SoftSkillsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SoftSkillsViewController.swift
+//  Homework-3
+//
+//  Created by Анастасия Ларина on 05.05.2022.
+//
+
+import UIKit
+
+class SoftSkillsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(displayP3Red: 241/255, green: 221/255, blue: 201/255, alpha: 1)
+        title = "Soft skills :"
+    }
+}
