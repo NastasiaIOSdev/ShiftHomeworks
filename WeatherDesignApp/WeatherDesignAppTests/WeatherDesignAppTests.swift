ac6ec4fb94137bc69dee7b889b7b146a3dffe3e4
AnastasiiaLarina <laranweb@gmail.com>
2022-05-26 15:24:50 +0500

    настройка build options

--- WeatherDesignApp/WeatherDesignAppTests/WeatherDesignAppTests.swift
+++ WeatherDesignApp/WeatherDesignAppTests/WeatherDesignAppTests.swift
@@ -8,29 +8,14 @@
 import XCTest
 @testable import WeatherDesignApp
 
-class WeatherDesignAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class DateConverterTests: XCTestCase {
+    func testFormatterCurrentDate() {
+        XCTAssertTrue(DateConverter.showDayWithWeekDay(Date.now).contains("Today"))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testFormatterOtherDate() {
+        let date = Date(timeIntervalSince1970: 1651513837)
+        print(date)
+        XCTAssertEqual(DateConverter.showDayWithWeekDay(date), "Mon, 2 May")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
