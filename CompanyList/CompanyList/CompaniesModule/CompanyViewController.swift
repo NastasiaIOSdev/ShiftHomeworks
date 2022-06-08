e049fff6b12c1d95c3dbc355c36e5e6211aa1d0a
AnastasiiaLarina <laranweb@gmail.com>
2022-06-08 12:31:58 +0500

    настройка scenedelegste

---
+++ CompanyList/CompanyList/CompaniesModule/CompanyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CompanyViewController.swift
+//  CompanyList
+//
+//  Created by Анастасия Ларина on 08.06.2022.
+//
+
+import UIKit
+
+class CompanyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 209/255, green: 32/255, blue: 35/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = UIColor(red: 209/255, green: 32/255, blue: 35/255, alpha: 1)
+        navigationController?.navigationBar.tintColor = .white
+    }
+
+
+}
+
