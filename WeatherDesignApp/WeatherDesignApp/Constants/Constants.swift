6656da5c3f8ea9f2303eb6d977ed8e049e43d9e8
AnastasiiaLarina <laranweb@gmail.com>
2022-06-09 13:21:57 +0500

    правки

--- WeatherDesignApp/WeatherDesignApp/Constants/Constants.swift
+++ WeatherDesignApp/WeatherDesignApp/Constants/Constants.swift
@@ -9,9 +9,8 @@ import Foundation
 
 struct Constants {
     static let apiKeyWeather = "1fbe35f21a364f7a9a384308220406"
-    static let city = "Bali"
-    static let zipCode = "07112"
+    static let city = "Novosibirsk"
+    static let date = "2022-06-9"
     static let weatherUrl = URL(string: "https://api.weatherapi.com/v1/current.json?key=\(apiKeyWeather)&q=\(city)")
-    static let searchUrl = "http://api.weatherapi.com/v1/search.json?key=\(apiKeyWeather)&q="
-    static let forecast7URL = "http://api.weatherapi.com/v1/forecast.json?key=\(apiKeyWeather)&q=\(zipCode)&days=7"
+    static let historyURL = "https://api.weatherapi.com/v1/history.json?key=\(apiKeyWeather)&q=\(city)&dt=\(date)"
 }
