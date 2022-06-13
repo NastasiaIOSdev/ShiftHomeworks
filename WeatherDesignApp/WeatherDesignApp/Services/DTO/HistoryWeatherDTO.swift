51b1264432b89f652120faebd8dbcbb8a14f6824
AnastasiiaLarina <laranweb@gmail.com>
2022-06-13 16:25:05 +0500

    настройка сетевого слоя для истории погоды

---
+++ WeatherDesignApp/WeatherDesignApp/Services/DTO/HistoryWeatherDTO.swift
@@ -0,0 +1,35 @@
+//
+//  HistoryWeatherDTO.swift
+//  WeatherDesignApp
+//
+//  Created by Анастасия Ларина on 13.06.2022.
+//
+
+import Foundation
+
+struct HistoryWeatherDTO: Codable {
+    let forecast: Forecast?
+}
+
+struct Forecast: Codable {
+    let forecastday: Forecastday?
+}
+
+struct Forecastday: Codable {
+    let date: String?
+    let day: Day?
+}
+
+struct Day: Codable {
+    let tempC: Double?
+    let condition: ConditionHistory?
+    
+    enum CodingKeys: String, CodingKey {
+        case tempC = "avgtemp_c"
+        case condition
+    }
+}
+
+struct ConditionHistory: Codable {
+    let icon: String?
+}
--- WeatherDesignApp/WeatherDesignApp/Services/WeatherService.swift
+++ WeatherDesignApp/WeatherDesignApp/Services/WeatherService.swift
@@ -20,6 +20,8 @@ class WeatherService {
         // + city + date
     }
     
+    // MARK: - CurrentWeather
+    
     func setNetworkProcessor(request networkRequest: String) {
         var request = networkRequest.trimmingCharacters(in: .whitespacesAndNewlines)
         request = networkRequest.replacingOccurrences(of: " ", with: "%20")
@@ -62,6 +64,78 @@ class WeatherService {
                 }
             } else {
                 completion(nil)
+            }
+        })
+    }
+    
+    // MARK: - History Weather
+    
+    func setNetworkProcessorHistory(request networkRequest: String) {
+        var request = networkRequest.trimmingCharacters(in: .whitespacesAndNewlines)
+        request = networkRequest.replacingOccurrences(of: " ", with: "%20")
+        let startDate = DateConverter.getDateDaysAgo(daysAgo: -7)
+        let currentDate = DateConverter.getDateDaysAgo(daysAgo: 0)
+        if let url = URL(string: "\(self.historyBaseURL!)&q=\(request)&dt=\(startDate)&end_dt=\(currentDate)") {
+            self.networkProcessor = NetworkProcessor(url: url)
+        }
+    }
+    
+    func getLocationHistort(_ location: String, completion: @escaping (Location?) -> Void) {
+        setNetworkProcessorHistory(request: location)
+        networkProcessor?.downloadJSONFronURL({ (jsonDictionary) in
+            if let weatherLocationDictionary = jsonDictionary?["location"] as? [String : Any] {
+                let location = Location(dictionary: weatherLocationDictionary)
+                completion(location)
+            } else {
+                completion(nil)
+            }
+        })
+    }
+    
+    func getHistortDay(_ location: String, completion: @escaping (HistoryForecastDay?) -> Void) {
+        setNetworkProcessorHistory(request: location)
+        networkProcessor!.downloadJSONFronURL({ (jsonDictionary) in
+            if let forecastDictionary = jsonDictionary?["forecast"] as? [String : Any] {
+                if let forecastDayDictionary = forecastDictionary["forecastday"] as? [String : Any] {
+                    let forecatDay = HistoryForecastDay(dictionary: forecastDayDictionary)
+                    completion(forecatDay)
+                }
+            } else {
+                completion(nil)
+            }
+        })
+    }
+    
+    func getHistoryTemperature(_ location: String, completion: @escaping (HistoryWeather?) -> Void) {
+        setNetworkProcessorHistory(request: location)
+        networkProcessor!.downloadJSONFronURL({ (jsonDictionary) in
+            if let forecastDictionary = jsonDictionary?["forecast"] as? [String : Any] {
+                if let forecastDayDictionary = forecastDictionary["forecastday"] as? [String : Any] {
+                    if let dayDictionary = forecastDayDictionary["day"] as? [String : Any] {
+                        let forecatDay = HistoryWeather(dictionary: dayDictionary)
+                        completion(forecatDay)
+                    }
+                } else {
+                    completion(nil)
+                }
+            }
+        })
+    }
+    
+    func getHistoryIcon(_ location: String, completion: @escaping (ConditionWeather?) -> Void) {
+        setNetworkProcessorHistory(request: location)
+        networkProcessor!.downloadJSONFronURL({ (jsonDictionary) in
+            if let forecastDictionary = jsonDictionary?["forecast"] as? [String : Any] {
+                if let forecastDayDictionary = forecastDictionary["forecastday"] as? [String : Any] {
+                    if let dayDictionary = forecastDayDictionary["day"] as? [String : Any] {
+                        if let conditionDictionary = dayDictionary["condition"] as? [String : Any] {
+                            let condition = ConditionWeather(dictionary: conditionDictionary)
+                            completion(condition)
+                        }
+                    } else {
+                        completion(nil)
+                    }
+                }
             }
         })
     }
