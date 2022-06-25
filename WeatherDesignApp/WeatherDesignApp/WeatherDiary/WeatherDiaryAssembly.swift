be35a15fe384ee925c7ec7c5ee452bd2843d40ab
AnastasiiaLarina <laranweb@gmail.com>
2022-06-25 11:48:13 +0500

    правки

---
+++ WeatherDesignApp/WeatherDesignApp/WeatherDiary/WeatherDiaryAssembly.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherDiaryAssembly.swift
+//  WeatherDesignApp
+//
+//  Created by Анастасия Ларина on 25.06.2022.
+//
+
+import UIKit
+
+enum WeatherDaiaryAssembly {
+    static func build() -> WeatherDiaryViewController {
+        let router = WeatherDiaryRouter()
+        let interactor = WeatherDiaryInteractor()
+        interactor.networkService = NetworkService()
+        let presenter = WeatherDiaryPresenter(interactor: interactor, router: router)
+        let vc = WeatherDiaryViewController(presenter: presenter)
+        router.vc = vc
+        return vc
+    }
+}
